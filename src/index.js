ef10c3f190ddf192f0ad6993a7bbcda5a5e13bb5
Andrew-aka-Lemon <105217029+Andrew-aka-Lemon@users.noreply.github.com>
2022-10-15 21:24:25 +0300

    some try

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import refs from './js/refs';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+
+const Axios = require('axios');
---
+++ src/js/refs.js
@@ -0,0 +1,4 @@
+export const refs = {
+  input: document.querySelector('[data-searh-input]'),
+  gallery_block: document.querySelector('.gallery'),
+};
